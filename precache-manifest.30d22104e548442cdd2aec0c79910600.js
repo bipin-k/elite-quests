b1b8d838df1ca6c69396724e67c03e7c78053d12
Bipin Kumar Chaurasia <bipin.biz@gmail.com>
2022-01-30 15:46:22 +0530

    Updates

---
+++ precache-manifest.30d22104e548442cdd2aec0c79910600.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4aedf62beffe04ff0b933c45f0485ab",
+    "url": "/elite-quests/index.html"
+  },
+  {
+    "revision": "e58dbaba5670b6d9a393",
+    "url": "/elite-quests/static/js/2.a47a98fc.chunk.js"
+  },
+  {
+    "revision": "7c413d2983a9b5fa76c7",
+    "url": "/elite-quests/static/js/main.aa62b10e.chunk.js"
+  },
+  {
+    "revision": "ca37ac837dfe7de4f094",
+    "url": "/elite-quests/static/js/runtime~main.b2afe5b7.js"
+  },
+  {
+    "revision": "014057176db41b1f937f27d599622d96",
+    "url": "/elite-quests/static/media/company.01405717.png"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/elite-quests/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/elite-quests/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/elite-quests/static/media/svg-3.397c455a.svg"
+  }
+]);
